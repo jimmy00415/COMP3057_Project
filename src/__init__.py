41cc929775360a817cbbd75a16c54200c4004f27
CHEN YIWEI <22256024@life.hkbu.edu.hk>
2025-10-27 11:26:50 +0800

    Fix import errors in src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,3 @@
 
 __version__ = "1.0.0"
 __author__ = "COMP3057 Project"
-
-from .data import *
-from .models import *
-from .training import *
-from .evaluation import *
-from .inference import *
-from .utils import *
